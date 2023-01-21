fd300ef386abc2512edee4ef4e08a32ee7626ff7
Dennis Maier <dennis@dennis-maier.de>
2023-01-21 11:23:45 +0100

    added rabbitmq

--- server/lib/Celery.js
+++ server/lib/Celery.js
@@ -2,8 +2,10 @@ const { config } = require("./config/config");
 import Logger from "./logger/Logger";
 const logger = new Logger("Celery");
 
-const CELERY_BROKER_URL = "amqp://user:mypass@rabbitmq:5672";
-const CELERY_RESULT_BACKEND = "redis://myredis-headless:6379/0";
+const CELERY_BROKER_URL = "amqp://user:mypass@10.243.64.9:31001";
+//const CELERY_BROKER_URL = "amqp://user:mypass@rabbitmq:5672";
+const CELERY_RESULT_BACKEND = "redis://10.243.64.9:6379/0";
+//const CELERY_RESULT_BACKEND = "redis://myredis-headless:6379/0";
 
 class Celery {
   constructor(roomId) {
