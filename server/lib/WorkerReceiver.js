1d91963897dabe08e1ecb00ed6a6b16588e35840
Dennis Maier <dennis@dennis-maier.de>
2023-01-22 13:40:34 +0100

    test data

--- server/lib/WorkerReceiver.js
+++ server/lib/WorkerReceiver.js
@@ -39,26 +39,26 @@ class WorkerSocketServer {
           // TODO REFACTOR THIS!!
           //emotion_classes = {0: "Neutral", 1: "Happy", 2: "Sad", 3:"Surprise", 4: "Fear", 5: "Disgust", 6: "Anger", 7: "Contempt"}
           //const final_result = { emotions:[{raw: patientResult["emotions"], 'dominantEmotion':'happy'}], "boxes": [patientResult["relativeBox"]] }
-          const final_result = {
-            emotions: [
-              {
-                raw: {
-                  neutral: patientResult["emotions"][0].value,
-                  happy: patientResult["emotions"][1].value,
-                  sad: patientResult["emotions"][2].value,
-                  surprise: patientResult["emotions"][3].value,
-                  fear: patientResult["emotions"][4].value,
-                  disgust: patientResult["emotions"][5].value,
-                  anger: patientResult["emotions"][6].value,
-                  contempt: patientResult["emotions"][7].value,
-                },
-                dominantEmotion: "happy",
-              },
-            ],
-            boxes: [patientResult["relativeBox"]],
-          };
-          //, 'dominantEmotion':emotion_classes[patientResult["emotions"].indexOf(Math.max(...patientResult["emotions"]))]}], "boxes": [patientResult["relativeBox"]] }
-
+          const final_result = { 
+            'output': "emotion_model_output", 
+            'emotions': { 
+              'userId': patientResult.usr, 
+              'emotions': [{
+                'raw':{
+                  'happy': {'date': Date.now(), 'value': 1}, 
+                  'sad': {'date': Date.now(), 'value': 0},
+                  'neutral': {'date': Date.now(), 'value': 0},
+                  'surprise': {'date': Date.now(), 'value': 0},
+                  'fear': {'date': Date.now(), 'value': 0},
+                  'disgust': {'date': Date.now(), 'value': 0},
+                  'anger': {'date': Date.now(), 'value': 0},
+                  'contempt': {'date': Date.now(), 'value': 0}
+                }, 
+                'dominantEmotion':'happy'
+              }], 
+              'boxes': [patientResult["relativeBox"]] 
+            } 
+          }
           console.log("sending result: ", final_result);
           socketio
             .getio()
